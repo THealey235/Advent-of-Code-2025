a2e4e9823e9dba6b8f5c487c434a75f62e41c2c8
THealey235 <122405008+THealey235@users.noreply.github.com>
2025-12-03 19:09:21 +0000

    Day 3
    Very happy to only have to use 1 function for both parts.
    
    An interesting interaction happens where this runs faster in debug mode and the function called second always runs significantly faster.
    
    Changed the naming scheme so that it is properly ordered for days > 9

---
+++ Day 03/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+
+namespace Day_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input;
+            using (var reader = new StreamReader("Input.txt"))
+            {
+                input = reader.ReadToEnd().ReplaceLineEndings().Split("\r\n").ToList();
+            }
+            var watch = new Stopwatch();
+
+            long output;
+
+            watch.Start();
+            output = FindJolts(input, 2);
+            watch.Stop();
+            Console.WriteLine($"Part 1: {output}, Time: {watch.Elapsed.TotalMicroseconds} μs");
+
+            watch.Restart();
+            output = FindJolts(input, 12);
+            watch.Stop();
+            Console.WriteLine($"Part 2: {output}, Time: {watch.Elapsed.TotalMicroseconds} μs");
+        }
+
+        static long FindJolts(List<string> banks, int batteries)
+        {
+            var digits = Enumerable.Range(1, 9).Select(x => x.ToString()[0]).ToArray();
+            long output = 0;
+            List<char> bankList;
+            foreach (var bank in banks)
+            {
+                int[] jolts = Enumerable.Repeat(0, batteries).ToArray();
+
+                int left = 0;
+                int right = batteries;
+                int a;
+
+                for (int i = 0; i < batteries; i++)
+                {
+                    right--;
+                    bankList = bank.Substring(left, bank.Length - right - left).ToList();
+                    a = 0;
+                    for (int j = 8; j >= 0; j--)
+                    {
+                        a = bankList.IndexOf(digits[j]);
+                        if (a != -1)
+                        {
+                            jolts[i] = bankList[a] - '0';
+                            break;
+                        }
+                    }
+                    left += a + 1;
+                    
+                }
+                output += long.Parse(String.Join(String.Empty, jolts));
+            }
+
+            return output;
+        }
+    }
+}
